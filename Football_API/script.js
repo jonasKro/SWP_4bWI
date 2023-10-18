2d580f63dda27bc65e322ef0e9e872de08cdbeef
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2023-10-18 13:54:17 +0200

    Seite umgebaut

--- Football_API/script.js
+++ Football_API/script.js
@@ -17,15 +17,27 @@ const loadData = () => {
 }
 loadData();
 
-const statlist = document.getElementById("statlist");
+
 //statlist.style.display = "none";
 const filltable = (data) => {
 
     let html = "";
     data.forEach(element => {
-        html += '<div class="teamoverview">' + element.teamName + " " + element.points + "</div>";
+        /*
+                html += '<div class="teamoverview">' + '<img src="' + element.teamIconURl + '">' + "</img>" + element.teamName + "    " + '<div class="points">' +
+            element.points + "</div>" + '<img id="buttonimg" src="button.jpg" class="buttonimg" onclick="toggleStats()">' + '</img>' + "</div>";
         html += '<ul class="teamstats" id="statlist">' + "<li>" + element.draw + "</li>" + "<li>" + element.won + "</li>" + "<li>" + element.lost + "</li>" + "</ul>"
-        html += '<img id="buttonimg" src="">'
+
+        */
     });
     document.getElementById("output").innerHTML = html;
+}
+
+
+function toggleStats() {
+    document.getElementById("statlist").classList.toggle("show");
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
